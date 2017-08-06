75d6bacd03346d668cf707aeb962b51c610a416f
Luis <lalvarezguillen@gmail.com>
2017-08-06 13:10:28 -0400

    initial sketch

---
+++ README.md
@@ -0,0 +1,1 @@
+This a simple nodejs project to test now.sh
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function getTimeAsText (datetime_obj){
+const express = require('express')
+const num2word = require('numbers2words')
+
+const app = express()
+const translator = new num2word('EN_US')
+
+app.get('/', function (req, res) {
+    res.send('Simple aplication to test now.sh')
+})
+
+app.get('/now', function (req, res) {
+    const now = new Date()
+    res.send(getTimeAsText(now))
+})
+
+app.listen(3000, function() {
+    console.log('Listening on port 3000')
+})
+
+
+function getTimeAsText (datetime_obj) {
     /*
     @description: Produces a readable time string from a datetime object.
 
@@ -7,26 +27,26 @@ function getTimeAsText (datetime_obj){
 
     @return: {str} A human-readable time string
     */
-    let base_12_hours, am_pm, minutes_string
+    let hours_str, am_pm, minutes_str
     if(datetime_obj.getHours() < 12){
-        base_12_hours = datetime_obj.getHours()
+        hours_str = translator.toWords(datetime_obj.getHours())
         am_pm = 'AM'
     }
     else if(datetime_obj.getHours() == 12){
-        base_12_hours = datetime_obj.getHours()
+        hours_str = translator.toWords(datetime_obj.getHours())
         am_pm = 'PM'
     }
     else{
-        base_12_hours = datetime_obj.getHours() - 12
+        hours_str = translator.toWords(datetime_obj.getHours() - 12)
         am_pm = 'PM'
     }
 
     if(datetime_obj.getMinutes() == 0){
-        minutes_string = 'o\'clock'
+        minutes_str = 'o\'clock'
     }
     else{
-        minutes_string = datetime_obj.getMinutes()
+        minutes_str = translator.toWords(datetime_obj.getMinutes())
     }
 
-    return ['It is', base_12_hours, minutes_string, am_pm].join(' ')
+    return ['It is', hours_str, minutes_str, am_pm].join(' ')
 }
