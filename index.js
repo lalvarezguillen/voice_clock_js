b9f27af29dc68862dc9d1ec9bdf0dc63d37b31d6
Luis <lalvarezguillen@gmail.com>
2017-08-06 21:18:54 -0400

    included ability to try to localize time, failing that it defaults to Greenwich

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const http = require('http')
 const https = require('https')
+const moment = require('moment-timezone')
 const Request = require('request')
 const express = require('express')
 const num2word = require('numbers2words')
@@ -8,17 +10,38 @@ const app = express()
 const translator = new num2word('EN_US')
 
 app.get('/', function (req, res) {
-    res.send('<p>Simple aplication to test now.sh</p><p>Make a request to /now to get current time</p>')
+    res.send('<p>Simple aplication to test now.sh</p>'
+             + '<p>Make a request to <a href="/now">/now</a>'
+             + ' to attempt to get current time in your timezone</p>'
+             + '<p>If that fails, it will display UTC time</p>')
 })
 
 app.get('/now', function (req, res) {
-    const now = new Date()
-    const datetime_str = getTimeAsText(now)
-
-    console.log(datetime_str)
-    googleTTS(datetime_str, 'en', 1)
-        .then(url => handleGoogleAudio(url, req, res))
+    console.log(req.headers['x-forwarded-for'])
+    getLocalizedDatetime(req['headers']['x-forwarded-for'])
+        .then(datetime_str => googleTTS(datetime_str, 'en', 1))
+        .catch(datetime_str => googleTTS(datetime_str, 'en', 1))
+            .then(url => handleGoogleAudio(url, req, res))
 })
+
+function getLocalizedDatetime(ip){
+    return new Promise((resolve, reject) =>{
+        Request.get("http://ip-api.com/json/"+ip, (err, resp, body) =>{
+            json_body = JSON.parse(body)
+            if(resp.statusCode == 200 && json_body.timezone){
+                const now = moment(new Date()).tz('UTC')
+                console.log(json_body.timezone)
+                const localized_dt = now.clone().tz(json_body.timezone)
+                console.log(localized_dt)
+                resolve(getTimeAsText(localized_dt, json_body.timezone))
+            }
+            else {
+                const now = moment(new Date()).tz('UTC')
+                reject(getTimeAsText(now, null))
+            }
+        })
+    })
+}
 
 function handleGoogleAudio (url, user_req, user_resp) {
     console.log(url)
@@ -39,7 +62,7 @@ app.listen(3000, function() {
 })
 
 
-function getTimeAsText (datetime_obj) {
+function getTimeAsText (datetime_obj, timezone) {
     /*
     @description: Produces a readable time string from a datetime object.
 
@@ -48,26 +71,33 @@ function getTimeAsText (datetime_obj) {
 
     @return: {str} A human-readable time string
     */
+    if (timezone){
+        var tz_str = 'in ' + timezone.split('/')[1]
+    }
+    else {
+        var tz_str = 'in Greenwhich'
+    }
+    
     let hours_str, am_pm, minutes_str
-    if(datetime_obj.getHours() < 12){
-        hours_str = translator.toWords(datetime_obj.getHours())
+    if(datetime_obj.hours() < 12){
+        hours_str = translator.toWords(datetime_obj.hours())
         am_pm = 'AM'
     }
-    else if(datetime_obj.getHours() == 12){
-        hours_str = translator.toWords(datetime_obj.getHours())
+    else if(datetime_obj.hours() == 12){
+        hours_str = translator.toWords(datetime_obj.hours())
         am_pm = 'PM'
     }
     else{
-        hours_str = translator.toWords(datetime_obj.getHours() - 12)
+        hours_str = translator.toWords(datetime_obj.hours() - 12)
         am_pm = 'PM'
     }
 
-    if(datetime_obj.getMinutes() == 0){
+    if(datetime_obj.minutes() == 0){
         minutes_str = 'o\'clock'
     }
     else{
-        minutes_str = translator.toWords(datetime_obj.getMinutes())
+        minutes_str = translator.toWords(datetime_obj.minutes())
     }
 
-    return ['It is', hours_str, minutes_str, am_pm].join(' ')
+    return ['It is', hours_str, minutes_str, am_pm, tz_str].join(', ')
 }
